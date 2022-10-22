1ef41b1674b97fd633517139c2320e734ca31aae
Mette <mette.ghijsen@student.fontys.nl>
2022-10-22 13:02:59 +0200

    Begin gemaakt aan countdowntimer scherm

---
+++ app/src/main/java/com/example/letsflex/CountdownTimer.kt
@@ -0,0 +1,40 @@
+package com.example.letsflex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatTextView
+
+class CountdownTimer : AppCompatActivity() {
+
+    private lateinit var timer: CountDownTimer
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_countdown_timer)
+
+        val textView = findViewById<TextView>(R.id.textView)
+        val button = findViewById<Button>(R.id.button)
+
+        object : CountDownTimer(5_000, 1){
+            override fun onTick(remaining: Long) {
+                textView.text = remaining.toString()
+            }
+            override fun onFinish() {
+                textView.text = "Done!"
+            }
+        }
+    }
+
+    override fun onStart(){
+        super.onStart()
+        timer.start()
+    }
+
+    override fun onStop(){
+        super.onStop()
+        timer.cancel()
+    }
+}
